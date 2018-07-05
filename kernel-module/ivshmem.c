9c3385622ca8e0353787c88201412112321012e0
Gavin <gavin.cui@mail.utoronto.ca>
2018-07-05 12:05:25 -0400

    add some basic structure

---
+++ kernel-module/ivshmem.c
@@ -0,0 +1,63 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/pci.h>
+
+#define DRIVER_NAME "ivshmem"
+#define IVSHMEM_VENDOR_ID 0x1AF4
+#define IVSHMEM_DEVICE_ID 0x1110
+
+
+static int ivshmem_probe(struct pci_dev *dev, const struct pci_device_id *id)
+{
+  printk(KERN_INFO "Probe function get called\n");
+  return 0;
+}
+
+static void ivshmem_remove(struct pci_dev *dev)
+{
+  printk(KERN_INFO "Remove function get called\n");
+  return;
+}
+
+ static struct pci_device_id ivshmem_pci_ids[] = {
+ 	{PCI_DEVICE(IVSHMEM_VENDOR_ID, IVSHMEM_DEVICE_ID)},
+ 	{ /* end: all zeroes */ }
+ };
+
+
+ static struct pci_driver ivshmem_pci_driver = {
+    .name = DRIVER_NAME,
+    .id_table = ivshmem_pci_ids,
+    .probe = ivshmem_probe,
+    .remove = ivshmem_remove,
+};
+
+
+static int __init ivshmem_init_module(void)
+{
+	int ret;
+  ret = pci_register_driver(&ivshmem_pci_driver);
+  if (ret == 0)
+  {
+    printk(KERN_INFO "Module loaded\n");
+  }
+  else
+  {
+    printk(KERN_ERR "Module failed to loaded\n");
+  }
+	return ret;
+}
+
+static void __exit ivshmem_exit_module(void)
+{
+  pci_unregister_driver(&ivshmem_pci_driver);
+  printk(KERN_INFO "Module exit");
+}
+
+MODULE_DEVICE_TABLE(pci, ivshmem_pci_ids);
+MODULE_DESCRIPTION("A simple ivshmem PCI driver");
+MODULE_AUTHOR("Gavin");
+MODULE_LICENSE("GPL");
+module_init(ivshmem_init_module);
+module_exit(ivshmem_exit_module);
